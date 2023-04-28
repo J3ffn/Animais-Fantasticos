26a624af2cc4819304c78da24c0246d5134cfaea
J3ffn <jefferson.mangueira12@gmail.com>
2023-04-28 19:36:50 -0300

    Adição de arquivos

---
+++ script.js
@@ -0,0 +1,22 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.removeAttribute("class");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
