93aebd29188c1d0efe49f0332a9ba898ed959a53
J3ffn <jefferson.mangueira12@gmail.com>
2023-04-28 22:24:18 -0300

    add: scrolling animation

--- script.js
+++ script.js
@@ -69,3 +69,26 @@ function scrollSuave() {
 }
 
 scrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.6;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        console.log(sectionTop);
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
